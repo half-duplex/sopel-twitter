b8e8001aa69c774e928f441ac8dfcdfdf4c5b818
dgw <dgw@technobabbl.es>
2020-12-18 12:53:45 -0700

    Add mobile subdomain handling to `/i/web/status` links too

--- sopel_modules/twitter/twitter.py
+++ sopel_modules/twitter/twitter.py
@@ -142,7 +142,7 @@ def format_time(bot, trigger, stamp):
 
 
 @module.url(r'https?://(?:m(?:obile)\.)?twitter\.com/(?P<user>[^/]+)(?:$|/status/(?P<status>\d+)).*')
-@module.url(r'https?://twitter\.com/i/web/status/(?P<status>\d+).*')
+@module.url(r'https?://(?:m(?:obile)\.)?twitter\.com/i/web/status/(?P<status>\d+).*')
 def get_url(bot, trigger, match):
     things = match.groupdict()
     user = things.get('user', None)
